112bd96dff99814fb00d867649cfc5955c47c487
Nobuyuki Takahiro <studio.quad9@gmail.com>
2022-10-05 05:53:57 +0900

    first commit

---
+++ themes4Homebrew#01/index.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Document</title>
+  <link rel="stylesheet" href="./assets/style.css">
+</head>
+<body>
+  <header>
+    homebrew
+  </header>
+  <main>
+    contents
+  </main>
+  <footer>
+    footer
+  </footer>
+  
+</body>
+</html>
